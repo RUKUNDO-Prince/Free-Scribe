9b2e875c26fa4b8b9d84634b29083326f44fcca9
RUKUNDO Prince <rukundoprince951@gmail.com>
2025-01-12 02:12:18 +0200

    finished the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import './index.css'
+import HomePage from './components/HomePage'
+import Header from './components/Header'
 
 const App = () => {
   return (
-    <div class>App</div>
+    <div className='flex flex-col max-w-[1000px] mx-auto w-full'>
+      <section className='min-h-screen flex flex-col'>
+        <Header />
+        <HomePage />
+      </section>
+      <h1 className='text-green-400'>hello</h1>
+      <footer></footer>
+    </div>
   )
 }
 
